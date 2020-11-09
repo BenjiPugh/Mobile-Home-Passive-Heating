dd51831b2e871d962a0df09402fc4edbbe3c477d
BenjiPugh <pughbenji@gmail.com>
2020-11-09 10:47:04 -0500

    Fixed Surface area wrong value

--- eulerTest.m
+++ eulerTest.m
@@ -7,22 +7,20 @@ volumeAir = 299;    %m^3 Standard volume of midsized manufactured home (4.27x17.
 massAir = densAir * volumeAir;  %kg
 
 t0 = 0;
-tend = 1;      %days
+tend = 86400;      %seconds
 
-dt = 1/(24*12);
+dt = 300;
 
 numSteps = (tend - t0) / dt;
 T = zeros(numSteps + 1, 1);
 
 U = zeros(size(T));
 U(1) = temperatureToEnergy(tAir, massAir, specAir);
-
-heatLost = NaN(numSteps + 1,1);
 for i = 1:numSteps
     % heat lost in watts (J/s)
-    heatLost(i) = heatLoss(energyToTemperature(U(i), massAir, specAir), tOutside);
-    % convert W to J
-    dudt = heatLost(i)*dt*86400;
+    heatLost = heatLoss(energyToTemperature(U(i), massAir, specAir), tOutside);
+    % convert W to 
+    dudt = heatLost*dt;
     T(i+1) = T(i) + dt;
     U(i+1) = U(i) + dudt;
 end
--- heatLoss.m
+++ heatLoss.m
@@ -5,7 +5,7 @@ function rate = heatLoss(indoorT, outdoorT)
     
     %Standard Mid-Sized manufactured home is 14x56x13.5ft or
     %4.27x17.06x4.11m
-    area = 7387;   %m^2
+    area = 4.27*4.11*2 + 17.06*4.11*2 + 4.27*17.06;   %m^2
     dUdt = (outdoorT - indoorT) * area / rVal;      %in Watts
     rate = dUdt;
 end
