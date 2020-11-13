8addfc51172c8bface7f0198d130e0482a87f47a
VanMyers <vanmmyers@gmail.com>
2020-11-12 22:58:46 -0500

    final model

--- heatLoss.m
+++ heatLoss.m
@@ -1,7 +1,7 @@
 function rate = heatLoss(indoorT, outdoorT)
     %Using refrences for standard mobile homes in the northeast, 40 K*m^2/W
     %is a recommended standard
-    rVal = 1.7;      %K*m^2/W
+    rVal = 3;      %K*m^2/W
     
     %Standard Mid-Sized manufactured home is 14x56x13.5ft or
     %4.27x17.06x4.11m
